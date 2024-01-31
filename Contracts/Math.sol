8a59427bc8a85bb04fa71048116408e6c0320756
S-Samiksha <samikshasankar01@gmail.com>
2024-01-31 12:33:03 +0800

    Simulations and Math solidity

--- Contracts/Math.sol
+++ Contracts/Math.sol
@@ -3,16 +3,15 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ * @title Math
+ * @dev For the mathematical functions required in the project
  */
 
 import "https://github.com/Vectorized/solady/blob/main/src/utils/FixedPointMathLib.sol";
 using FixedPointMathLib for uint256;
 using FixedPointMathLib for int256;
 
-contract Storage {
+contract Math {
     uint256 number;
 
     function mul(uint256 a, uint256 b) public pure returns (uint256 c) {
@@ -58,29 +57,86 @@ contract Storage {
         return c;
     }
 
-    function sigmoidalFunc(int256 x) public pure returns (uint256 rep) {
+    function sigmoidalFunc(
+        int256 x,
+        uint256 a_int,
+        int256 b_int,
+        int256 c_int
+    ) public pure returns (uint256 rep) {
+        //check the 0 condition
         if (x == b_int) {
             rep = a_int * (10 ** 18);
+            return rep;
         }
+
+        //convert to fixedpointmathlib format
         x = x * (10 ** 18);
-        uint256 a = 50 * (10 ** 18);
-        int256 b = 500 * (10 ** 18);
-        int256 c = 35000 * (10 ** 18);
+        uint256 a = a_int * (10 ** 18);
+        int256 b = b_int * (10 ** 18);
+        int256 c = c_int * (10 ** 18);
+
         //1. calculate (x-b) then abs then power
         int256 temp1 = pow0(int256(abs2(sub(x, b))), 2 * (10 ** 18));
+
         //2. calculate add c then sqrt
         uint256 temp2 = sqrt(uint256(add(temp1, c)));
+
         //3. if x-b is more than 0 do the normal division
         if (sub(x, b) > 0) {
             uint256 temp3 = div(uint256(sub(x, b)), temp2);
             uint256 temp4 = uint256(add(int256(temp3), 1 * (10 ** 18)));
             uint256 temp5 = mul(temp4, a);
             rep = temp5;
+            return rep;
         } else if (sub(x, b) < 0) {
             uint256 temp3 = div(uint256(abs2(sub(x, b))), temp2);
             uint256 temp4 = mul(temp3, a);
             uint256 temp5 = uint256(sub(int256(a), int256(temp4)));
             rep = temp5;
+            return rep;
+        }
+    }
+
+    function deltaT(
+        int256 delta_T,
+        uint256 xold_int,
+        int256 b_int,
+        int256 beta2_int
+    ) public pure returns (uint256 rep) {
+        //by default b_int is 12
+
+        //check the 0 condition
+        if (delta_T == b_int) {
+            rep = 0 * (10 ** 18);
+            return rep;
+        }
+
+        //convert to fixedpointmathlib format
+        delta_T = delta_T * (10 ** 18);
+        uint256 xold = xold_int * (10 ** 18);
+        int256 b = b_int * (10 ** 18);
+        int256 beta2 = beta2_int * (10 ** 18);
+
+        if (delta_T > b) {
+            //1. calculate (x-b) then abs then power
+            int256 temp1 = pow0(int256(abs2(sub(delta_T, b))), 2 * (10 ** 18));
+
+            //2. calculate add c then sqrt
+            uint256 temp2 = sqrt(uint256(add(temp1, beta2)));
+
+            uint256 temp3 = div(uint256(sub(delta_T, b)), temp2);
+            rep = mul(temp3, xold);
+            return rep;
+        } else if (b > delta_T) {
+            //1. calculate (x-b) then abs then power
+            int256 temp1 = pow0(int256(abs2(sub(b, delta_T))), 2 * (10 ** 18));
+
+            //2. calculate add c then sqrt
+            uint256 temp2 = sqrt(uint256(add(temp1, beta2)));
+
+            uint256 temp3 = div(uint256(abs2(sub(b, delta_T))), temp2);
+            rep = mul(temp3, xold);
+            return rep;
         }
     }
 }
